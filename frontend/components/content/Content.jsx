507ab8b5aea30eb867326fad680a9945adae05e3
AbhishekJha-45 <krjha36555@gmail.com>
2024-08-11 20:15:02 +0530

    fixed home

--- frontend/components/content/Content.jsx
+++ frontend/components/content/Content.jsx
@@ -8,7 +8,12 @@ import { BsCart2 } from "react-icons/bs";
 import BASE_URL from "../../constants/constants";
 import { setProducts as setStoreProducts } from "../../store/slice/productsSlice";
 import { useDispatch, useSelector } from "react-redux";
-import { addToCart } from "../../store/slice/cartSlice";
+import {
+  addToCart,
+  decrementQuantity,
+  deleteFromCart,
+} from "../../store/slice/cartSlice";
+import getToken from "utils/getToken";
 
 function Content({ className }) {
   const dispatch = useDispatch();
@@ -17,6 +22,8 @@ function Content({ className }) {
 
   const isProductsStored = useSelector((state) => state.products.products);
   const isExistingProducts = isProductsStored && isProductsStored.length > 0;
+  const quantity = useSelector((state) => state.cart.cart);
+  console.log(quantity);
   useEffect(() => {
     // console.log("res", products);
     const fetchProducts = async () => {
@@ -24,7 +31,7 @@ function Content({ className }) {
         const res = await axios.get(`${BASE_URL}/product/get-products`, {
           withCredentials: true,
         });
-        console.log(res.data.data.products);
+        // console.log(res.data.data.products);
         if (res.status === 200) {
           setProducts(res.data.data.products);
           dispatch(setStoreProducts(res.data.data.products));
@@ -43,6 +50,40 @@ function Content({ className }) {
     }
   }, [isExistingProducts, isFetched]);
 
+  const handleAddToCart = async (product) => {
+    dispatch(
+      addToCart({
+        _id: product._id,
+        name: product.name,
+        description: product.description,
+        price: product.price,
+        image: product.image,
+      })
+    );
+    const qty = quantity.find((item) => {
+      if (item._id === product._id) {
+        return item.quantity;
+      }
+    });
+    const res = await axios.post(
+      `${BASE_URL}/cart/add-to-cart`,
+      {
+        product_id: product._id,
+        quantity: qty.quantity,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${getToken("access_token")}`,
+        },
+      }
+    );
+    if (res.status === 201 || res.status === 200) {
+      return alert("Successfully added to cart");
+    } else {
+      return alert("Failed to add item");
+    }
+  };
+
   return (
     <section className={className + " lg:px-5"}>
       <Breadcrumb pageName={"products"} />
@@ -58,40 +99,43 @@ function Content({ className }) {
       </ul>
 
       <div className="my-8 flex flex-col gap-y-5">
-        {products.map((product) => (
-          <div
-            className="grid grid-cols-10 gap-x-4 border-t border-b border-stroke p-4"
-            key={product.product_id}
-          >
-            <Image
-              className="col-span-2 aspect-[4/3] object-center object-contain h-full w-full"
-              src={product.image}
-              width={200}
-              height={80}
-              alt={product.name}
-            />
-            <div className="col-span-5 text-sm flex flex-col">
-              <p>{product.name}</p>
-              <p className="flex gap-x-2 justify-start items-center pt-2">
-                <FaStar color="green" /> From {product.reviews} reviews
-              </p>
-              <p className="text-xs pt-2">{product.description}</p>
+        {products.map((product) => {
+          // console.log(product);
+          return (
+            <div
+              className="grid grid-cols-10 gap-x-4 border-t border-b border-stroke p-4"
+              key={product._id}
+            >
+              <Image
+                className="col-span-2 aspect-[4/3] object-center object-contain h-full w-full"
+                src={product.image}
+                width={200}
+                height={80}
+                alt={product.name}
+              />
+              <div className="col-span-5 text-sm flex flex-col">
+                <p>{product.name}</p>
+                <p className="flex gap-x-2 justify-start items-center pt-2">
+                  <FaStar color="green" /> From {product.reviews} reviews
+                </p>
+                <p className="text-xs pt-2">{product.description}</p>
+              </div>
+              <div className="col-span-3 text-sm flex flex-col gap-y-3 justify-center items-center">
+                <p>Price: ₹ {product.price}</p>
+                <p>Category: {product.category_name}</p>
+                <button
+                  onClick={() => handleAddToCart(product)}
+                  className="bg-orange-600 w-1/2 py-2 flex justify-center items-center gap-x-2 text-white focus:outline-none active:scale-95 transition-transform duration-150"
+                >
+                  Add to Cart <BsCart2 color="white" fontSize="large" />
+                </button>
+                <button className="bg-orange-600 w-1/2 py-2 flex justify-center items-center gap-x-2 text-white focus:outline-none active:scale-95 transition-transform duration-150">
+                  Buy Now
+                </button>
+              </div>
             </div>
-            <div className="col-span-3 text-sm flex flex-col gap-y-3 justify-center items-center">
-              <p>Price: ₹ {product.price}</p>
-              <p>Category: {product.category_name}</p>
-              <button
-                onClick={() => dispatch(addToCart(product.product_id))}
-                className="bg-orange-600 w-1/2 py-2 flex justify-center items-center gap-x-2 text-white focus:outline-none active:scale-95 transition-transform duration-150"
-              >
-                Add to Cart <BsCart2 color="white" fontSize="large" />
-              </button>
-              <button className="bg-orange-600 w-1/2 py-2 flex justify-center items-center gap-x-2 text-white focus:outline-none active:scale-95 transition-transform duration-150">
-                Buy Now
-              </button>
-            </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </section>
   );
--- frontend/store/slice/cartSlice.js
+++ frontend/store/slice/cartSlice.js
@@ -1,181 +1,50 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
-import Cookies from "js-cookie";
+
 const initialState = {
   cart: [],
-  total: 0,
 };
-const access_token = Cookies.get("access_token");
+
 const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
-    // addToCart: (state, action) => {
-    //   const item = action.payload;
-    //   const existingItem = state.cart.find(
-    //     (cartItem) => cartItem.item_id === item.item_id
-    //   );
-    //   if (existingItem) {
-    //     existingItem.quantity += 1;
-    //   } else {
-    //     state.cart.push({ ...item, quantity: 1 });
-    //   }
-    //   state.total = state.cart.reduce(
-    //     (total, item) => total + item.price * item.quantity,
-    //     0
-    //   );
-    // },
-    initialCart: (state, action) => {
-      state.cart = action.payload;
-      state.total = state.cart.reduce(
-        (total, item) => total + item.price * item.quantity,
-        0
+    addToCart: (state, action) => {
+      // console.log(action.payload);
+      const existingItem = state.cart.find(
+        (item) => item._id === action.payload._id
       );
-    },
-  },
-  extraReducers: (builder) => {
-    builder
-      .addCase(updateCartItem.pending, (state) => {
-        state.status = "loading";
-      })
-      .addCase(updateCartItem.fulfilled, (state, action) => {
-        console.table(action.payload);
-        state.status = "succeeded";
+
+      if (existingItem) {
         state.cart = state.cart.map((item) =>
-          item.item_id === action.payload.item_id
-            ? { ...item, quantity: action.payload.quantity }
+          item._id === action.payload._id
+            ? { ...item, quantity: item.quantity + 1 }
             : item
         );
-        state.total = state.cart.reduce(
-          (total, item) => total + item.price * item.quantity,
-          0
-        );
-      })
-      .addCase(updateCartItem.rejected, (state, action) => {
-        state.status = "failed";
-      });
+      } else {
+        state.cart.push({ ...action.payload, quantity: 1 });
+      }
+    },
+    decrementQuantity: (state, action) => {
+      const existingItem = state.cart.find(
+        (item) => item._id === action.payload._id
+      );
 
-    builder
-      .addCase(addToCart.pending, (state) => {
-        state.status = "loading";
-        console.log("adding to cart");
-      })
-      .addCase(addToCart.fulfilled, (state, action) => {
-        state.status = "succeeded";
-        const item = action.payload;
-        const existingItem = state.cart.find(
-          (cartItem) => cartItem.item_id === item.item_id
+      if (existingItem.quantity === 1) {
+        state.cart = state.cart.filter((item) => item._id !== action.payload._id);
+      } else {
+        state.cart = state.cart.map((item) =>
+          item._id === action.payload._id
+            ? { ...item, quantity: item.quantity - 1 }
+            : item
         );
-        if (existingItem) {builder
-          .addCase(updateCartItem.pending, (state) => {
-            state.status = "loading";
-          })
-          .addCase(updateCartItem.fulfilled, (state, action) => {
-            console.table(action.payload);
-            state.status = "succeeded";
-            state.cart = state.cart.map((item) =>
-              item.item_id === action.payload.item_id
-                ? (item.quantity = action.payload.quantity)
-                : { ...item }
-            );
-            state.total = state.cart.reduce(
-              (total, item) => total + item.price * item.quantity,
-              0
-            );
-          })
-          .addCase(updateCartItem.rejected, (state, action) => {
-            state.status = "failed";
-          });
-          console.log("item already exists");
-        } else {
-          state.cart.push({ ...item });
-        }
-        state.total = state.cart.reduce(
-          (total, item) => total + item.price * item.quantity,
-          0
-        );
-      })
-      .addCase(addToCart.rejected, (state, action) => {
-        state.status = "failed";
-        console.log("failed to add to cart");
-      });
-
-    builder
-      .addCase(deleteCartItem.pending, (state) => {
-        state.status = "loading";
-      })
-      .addCase(deleteCartItem.fulfilled, (state, action) => {
-        state.status = "succeeded";
-        state.cart = state.cart.filter(
-          (item) => item.item_id !== action.payload.item_id
-        );
-        state.total = state.cart.reduce(
-          (total, item) => total + item.price * item.quantity,
-          0
-        );
-      });
+      }
+    },
+    deleteFromCart: (state, action) => {
+      state.cart = state.cart.filter((item) => item._id !== action.payload._id);
+    },
   },
 });
 
-export const { incrementQuantity, decrementQuantity, initialCart } =
+export const { addToCart, decrementQuantity, deleteFromCart } =
   cartSlice.actions;
 export default cartSlice.reducer;
-
-export const updateCartItem = createAsyncThunk(
-  "cart/updateCartItem",
-  async ({ item_id, product_id, quantity }) => {
-    console.log(item_id, product_id, quantity);
-    const response = await axios.put(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/order/items/update-quantity`,
-      {
-        item_id,
-        quantity,
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${access_token}`,
-        },
-      }
-    );
-    // console.log(response.data);
-    return response.data;
-  }
-);
-
-export const addToCart = createAsyncThunk(
-  "cart/addToCart",
-  async (product_id) => {
-    console.log("adding to cart", product_id);
-    const response = await axios.post(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/cart/add-to-cart`,
-      {
-        product_id,
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${access_token}`,
-        },
-      }
-    );
-    return response.data.data;
-  }
-);
-
-export const deleteCartItem = createAsyncThunk(
-  "cart/deleteCartItem",
-  async (item_id) => {
-    const response = await axios.delete(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/order/items/delete-order-item`,
-      {
-        headers: {
-          Authorization: `Bearer ${access_token}`,
-        },
-        data: {
-          item_id,
-        },
-      }
-    );
-    return response.data;
-  }
-);
