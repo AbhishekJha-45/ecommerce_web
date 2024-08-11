72492c1d6d2779ec55c1bf5384125acfe647f228
AbhishekJha-45 <krjha36555@gmail.com>
2024-08-11 13:57:05 +0530

    update 11 aug

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -5,9 +5,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import Jwt from "jsonwebtoken";
 export const verifyJwt = asyncHandler(async (req, res, next) => {
   try {
-    const token =
-      req.cookies?.accessToken ||
-      req.header("Authorization")?.replace("Bearer ", "");
+    const token = req.header("Authorization")?.replace("Bearer ", "");
     if (!token) {
       return res.status(401).json(new APiResponse(401, "Unauthorized request"));
     }
