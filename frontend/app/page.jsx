17d607622a3e13936dcaa8e3625164e7e510d923
AbhishekJha-45 <krjha36555@gmail.com>
2024-09-09 11:27:43 +0530

    added new card

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -1,13 +1,16 @@
 import Footer from "../components/Common/Footer/Footer";
 import Navbar from "../components/Common/Navbar/Navbar";
 import Home from "../components/Home/Home";
-
+import ProductCard from "@components/Common/Cards/ProductCard";
 import Carousel from "../components/Home/Carousel";
 function page() {
   return (
     <main className="w-full">
       <Navbar />
       <Home />
+      <div className="p-5">
+        <ProductCard />
+      </div>
       <Footer />
     </main>
   );
