186ae24fba256101bda8a44069a699d00aa8622c
AbhishekJha-45 <krjha36555@gmail.com>
2024-10-06 20:03:34 +0530

    fixed conflicts

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -1,25 +1,14 @@
 import Footer from "../components/Common/Footer/Footer";
 import Navbar from "../components/Common/Navbar/Navbar";
 import Home from "../components/Home/Home";
-<<<<<<< HEAD
-
 
 async function page() {
-  return (
-    <main className="w-full">
-      <Navbar/>
-=======
-
-async function page() {
-
   return (
     <main className="w-full">
       <Navbar />
->>>>>>> origin/main
       <Home />
-      
-      <Footer />
 
+      <Footer />
     </main>
   );
 }
